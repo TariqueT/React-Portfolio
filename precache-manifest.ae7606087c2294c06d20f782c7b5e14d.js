6141d8e96ad2ca08025e2a1646d8f8626b8058ab
Tarique Thomas <tarique.thomas@outlook.com>
2020-05-20 12:12:08 -0500

    Updates

---
+++ precache-manifest.ae7606087c2294c06d20f782c7b5e14d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e5367f9e3d08803b74a59257f3284b",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "a3e92adabdaca1aa8deb",
+    "url": "/React-Portfolio/static/css/main.f2596962.chunk.css"
+  },
+  {
+    "revision": "4a0f9151bdf3007fb55d",
+    "url": "/React-Portfolio/static/js/2.fa6f5df4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.fa6f5df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e92adabdaca1aa8deb",
+    "url": "/React-Portfolio/static/js/main.c9855ba2.chunk.js"
+  },
+  {
+    "revision": "75a24f30da2fa81fb6a7",
+    "url": "/React-Portfolio/static/js/runtime-main.eb0a70d6.js"
+  }
+]);
